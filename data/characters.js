f199270a374882fe5026f35a0cf0ddd0ac88dcb1
Jorge Gonzalez <jglez.cs@gmail.com>
2022-03-05 21:59:58 -0600

    Adds all Ishigami Villagers characters

--- data/characters.js
+++ data/characters.js
@@ -13,8 +13,173 @@ const characters = [
   },
   {
     id: 4,
-    name: "Tsukasa Shishio"
+    name: "Tsukasa Shishio",
+  },
+  {
+    id: 5,
+    name: "Kohaku",
+  },
+  {
+    id: 6,
+    name: "Ruri"
+  },
+  {
+    id: 7,
+    name: "Suika"
+  },
+  {
+    id: 8,
+    name: "Ginro"
+  },
+  {
+    id: 9,
+    name: "Kinro"
+  },
+  {
+    id: 10,
+    name: "Magma"
+  },
+  {
+    id: 11,
+    name: "Mantle"
+  },
+  {
+    id: 12,
+    name: "Turquoise"
+  },
+  {
+    id: 13,
+    name: "Jasper"
+  },
+  {
+    id: 14,
+    name: "Kokuyo"
+  },
+  {
+    id: 15,
+    name: "Chrome"
+  },
+  {
+    id: 16,
+    name: "Kaseki"
+  },
+  {
+    id: 17,
+    name: "Ruby"
+  },
+  {
+    id: 18,
+    name: "Garnet"
+  },
+  {
+    id: 19,
+    name: "Sapphire"
+  },
+  {
+    id: 20,
+    name: "Sagan"
+  },
+  {
+    id: 21,
+    name: "En"
+  },
+  {
+    id: 22,
+    name: "Ganen"
+  },
+  {
+    id: 23,
+    name: "Kujaku"
+  },
+  {
+    id: 24,
+    name: "Shovel"
+  },
+  {
+    id: 25,
+    name: "Azura"
+  },
+  {
+    id: 26,
+    name: "Gen Asagiri"
+  },
+  {
+    id: 27,
+    name: "Carbo"
+  },
+  {
+    id: 28,
+    name: "Titan"
+  },
+  {
+    id: 29,
+    name: "Genbu"
+  },
+  {
+    id: 30,
+    name: "Alabaster"
+  },
+  {
+    id: 31,
+    name: "Namari"
+  },
+  {
+    id: 32,
+    name: "Chalk"
+  },
+  {
+    id: 33,
+    name: "Beryl"
+  },
+  {
+    id: 34,
+    name: "Argo"
+  },
+  {
+    id: 35,
+    name: "Soyuz"
+  },
+  {
+    id: 36,
+    name: "Hagane"
+  },
+  {
+    id: 37,
+    name: "Tetsuken"
+  },
+  {
+    id: 38,
+    name: "Shirogane"
+  },
+  {
+    id: 39,
+    name: "Natri"
+  },
+  {
+    id: 40,
+    name: "Alumi"
+  },
+  {
+    id: 41,
+    name: "Suzu"
+  },
+  {
+    id: 42,
+    name: "Unmo"
+  },
+  {
+    id: 43,
+    name: "Dia"
+  },
+  {
+    id: 44,
+    name: "Sho"
+  },
+  {
+    id: 45,
+    name: "Sango"
   }
+
 ];
 
 module.exports = characters;
