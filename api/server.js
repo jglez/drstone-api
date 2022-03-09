5c23920771caaee67015a0c1a3df33747e743072
Jorge Gonzalez <jglez.cs@gmail.com>
2022-03-08 22:29:29 -0600

    Adds React client, adds heroku-postbuild script, adds route for React app

--- api/server.js
+++ api/server.js
@@ -1,18 +1,17 @@
 const express = require("express");
+const path = require("path");
 const charactersRouter = require("./characters/characters-router.js");
 const episodesRouter = require("./episodes/episodes-router.js")
 
 const server = express();
+server.use(express.static(path.join(__dirname, "../client/build")));
 
 server.get("/api", (req, res) => res.json("Welcome to the Dr. Stone API. This is exhilarating - get excited!"));
 server.use("/api/characters", charactersRouter);
 server.use("/api/episodes", episodesRouter);
 
-server.use("*", (req, res) => { // catch-all
-  res.status(404).send(`
-    <h1>Dr. Stone API</h1>
-    <p>Oops, we can't find what you're looking for!</p>
-  `)
-})
+server.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 module.exports = server;
