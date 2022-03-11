63fbbfb5ab86097404239ddf474b3028ca42ff6a
Jorge Gonzalez <jglez.cs@gmail.com>
2022-03-10 21:39:47 -0600

    Removes React boilerplate, adds Home page component

---
+++ client/src/components/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Home() {
+  return(
+  <div id="home">
+    <nav>
+        <a>Docs</a>
+        <a>About</a>
+      </nav>
+
+    <header>
+      <h1>The Dr. Stone API</h1>
+    </header>
+  </div>
+  );
+}
+
+export default Home;
