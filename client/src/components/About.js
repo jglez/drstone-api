429196e786e151dab031b038669e940a0c101757
Jorge Gonzalez <jglez.cs@gmail.com>
2022-03-18 19:10:04 -0500

    Adds content to About page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,8 +1,22 @@
 function About() {
   return(
-    <>
-      <h1>About</h1>
-    </>
+    <div id="about-page">
+      <div id="about-container">
+
+        <h2>What is this?</h2>
+        <p>This is a REST API based on the manga and anime series <a target="_blank" href="https://dr-stone.fandom.com/wiki/Dr._Stone_(Anime)">Dr. Stone</a>, written by <a target="_blank" href="https://dr-stone.fandom.com/wiki/Riichiro_Inagaki">Riichiro Inagaki</a> and illustrated by <a target="_blank" href="https://dr-stone.fandom.com/wiki/Boichi">Boichi</a>. You have access to information on the most significant characters &#40;such as their occupation, affiliation, first appearance, etc.&#41;, their pictures, and basic information about the episodes.<br /><br />Disclaimer: This API is specifically for the anime as I haven't read the manga!</p>
+
+        <h2>Who are you?</h2>
+        <p>I'm Jorge, a guy from Chicago who loves learning about almost anything but mostly about technology and a little about psychology. I like watching anime in my spare time &#40;hence this project!&#41;</p>
+
+        <h2>Why did you build this?</h2>
+        <p>In Dr. Stone, the protagonist, Senku, finds himself in a primitive world where modern technology and civilization has decayed after waking up 3,700 years after all humans were petrified in stone. <br /><br />Using his love for problem-solving and science, Senku re-creates humanity's inventions from the ground-up to help a group of villagers he finds, struggling with issues such as illness due to pathogens. &#40;There's even an arc where Senku re-creates antibiotics using nothing but primitive tools in order to save someone!&#41; <br /><br /> Watching the show, Senku's passion for science and creating things reminded me of myself when I was really curious as a kid too, so I felt inspired and decided to build this in his honor!</p>
+
+        <h2>Copyright?</h2>
+        <p>Dr. Stone is created by Riichiro Inagaki and Boichi. The data and images are used without claim of ownership and belong to their respective owners.</p>
+
+      </div>
+    </div>
   )
 }
 
